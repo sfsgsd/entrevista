d2445255577b400aa3fe8326eebd39c96230cefd
sfsgsd <fdcuser@uesorl.com>
2018-10-31 07:29:08 -0400

    aplicacion de la entrevista

---
+++ Cliente/Cliente/Controllers/ClientesController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Cliente.Models;
+using PagedList;
+
+namespace Cliente.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private ClienteDBContext db = new ClienteDBContext();
+
+        // GET: Clientes
+        public ViewResult Index(string sortOrder, string currentFilter, string searchString, int ? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = "";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var clientes = from s in db.cliente select s;
+            
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                clientes = clientes.Where(s => s.primerNombre.Contains(searchString) || s.segundoNombre.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    clientes = clientes.OrderByDescending(s => s.primerNombre);
+                    break;
+                default:
+                    clientes = clientes.OrderBy(s => s.primerNombre);
+                    break;
+
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+
+            return View(clientes.ToPagedList(pageNumber, pageSize));
+           // return View(db.cliente.ToList());
+        }
+
+        // GET: Clientes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clientes clientes = db.cliente.Find(id);
+            if (clientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientes);
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clientes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,primerNombre,segundoNombre,direccion,telefono,correo")] Clientes clientes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.cliente.Add(clientes);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(clientes);
+        }
+
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clientes clientes = db.cliente.Find(id);
+            if (clientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientes);
+        }
+
+        // POST: Clientes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,primerNombre,segundoNombre,direccion,telefono,correo")] Clientes clientes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(clientes).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(clientes);
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clientes clientes = db.cliente.Find(id);
+            if (clientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientes);
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Clientes clientes = db.cliente.Find(id);
+            db.cliente.Remove(clientes);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Cliente/Cliente/Models/Clientes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Cliente.Models
+{
+    public class Clientes
+    {
+        public int id { get; set; }
+        [Required]
+        [Display (Name = "Primer Nombre")]
+        public string primerNombre { get; set; }
+        [Display(Name ="Segundo nombre")]
+        public string segundoNombre{ get; set; }
+        [Display (Name ="Direccion")]
+        public string direccion { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name ="Numero de Telefono")]
+        
+        public int telefono { get; set; }
+        
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display (Name ="Correo electronico")]
+        public string correo { get; set; }
+        
+    }
+
+    public class ClienteDBContext : DbContext {
+        public DbSet<Clientes> cliente { get; set; }
+    }
+
+
+}
